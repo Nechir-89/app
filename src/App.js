c0ead3b5fb3820ca7f98949feeedb4b0abee0cbb
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-06-17 00:05:38 -0400

    cleaning up master to be setup as template

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-// Modules
-import { Outlet } from 'react-router-dom';
-
-// Components
-import TopNavBar from './components/topNavBar';
-
 function App() {
   return (
-    <div style={appStyle}>
-      <h1>Bookkeeper</h1>
-      <TopNavBar />
-      <Outlet />
-    </div >
+    <div>
+      
+    </div>
   );
-}
-
-const appStyle = {
-  margin: '1rem'
 }
 
 export default App;
--- src/pages/notFoundPage/pageNotFound.jsx
+++
@@ -1,9 +0,0 @@
-function PageNotFound(){
-  return (
-    <main>
-      <h2>Page Not Found :( </h2>
-    </main>
-  )
-}
-
-export default PageNotFound;
