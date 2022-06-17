9349c8a6605447f5f1d481f1bca514dc8f0c1b42
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-06-17 04:16:48 -0400

    used useMemo and useCallback to fix expensive calculation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
+import { useEffect, useState, useMemo, useCallback } from 'react';
+
+const getArray = () => {
+  for (let i = 0; i < 1000000000; i++) {
+    //do something expensive
+  }
+  return ['Dave', 'Gray'];
+}
+
+
 
 function App() {
+  const [userNumber, setUserNumber] = useState('');
+  const [userName, setUserName] = useState('');
+
+  const fib = useCallback((n) => {
+    return (n <= 1) ? n : fib(n - 1) + fib(n - 2);
+  }, [])
+
+  const fibNumber = useMemo(() => fib(userNumber), [userNumber, fib]);
+
+  // const users = useMemo(() => getArray(), []);
+
+  useEffect(() => {
+    console.log(fibNumber);
+    console.log(fib);
+    console.log(userNumber);
+
+  }, [fibNumber, fib, userNumber])
+
   return (
-    <div>
-      Aplication
-    </div>
+    <main className="App">
+      <label>Fibonacci Sequence:</label>
+      <input
+        type="number"
+        value={userNumber}
+        placeholder="Position"
+        onChange={(e) => setUserNumber(e.target.value)} />
+
+      <p>Number: {fibNumber || "--"}</p>
+
+      <br /><br />
+      <label>Random Input:</label>
+      <input
+        type="text"
+        value={userName}
+        placeholder="Random Input"
+        onChange={(e) => setUserName(e.target.value)} />
+
+      <p>{userName}</p>
+    </main>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import Router from './routes/router.js'
-
+import App from './App'
+import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>
 );
 
