5671a34ce2ef0fa05c4fa74c96162c9f683bd93c
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-06-17 04:40:34 -0400

    adding warning message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 function App() {
   return (
-    <div>
-      
+    <div style={divStyle}>
+      You are in <strong>Main branch</strong>, please create new branch before starting off working or practicing
     </div>
   );
+}
+
+const divStyle = {
+  color: "yellow", 
+  backgroundColor: "black",
+  height: "92vh",
+  margin: 0,
+  padding: "1rem"
 }
 
 export default App;
