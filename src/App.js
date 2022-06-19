8a178360677ec5114a919997409108ac1969b357
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-06-19 12:58:49 -0400

    loging out component to the console

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React from 'react';
+
 function App() {
+  const [user, setUser] = React.useState('Neck');
+  console.log(<h1 className='a'>jsx element</h1>)
   return (
     <div style={divStyle}>
-      You are in <strong>Main branch</strong>, please create new branch before starting off working or practicing
+      You are in
+      <strong>Main branch</strong>
+      {user}, please create new branch before starting off working or practicing
     </div>
   );
 }
 
 const divStyle = {
-  color: "yellow", 
+  color: "yellow",
   backgroundColor: "black",
   height: "92vh",
   margin: 0,
