cc7ce365051864d201f8357908f7a23fe22e04d4
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-07-12 15:55:08 -0400

    providing components with context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import React from "react";
 import ThemeContext from "./contexts/theme";
 
 function App() {
   return (
-    <ThemeContext.Provider value="dark">
+    <ThemeContext.Provider value="light">
       <h1>context</h1>
+      <NavBar />
     </ThemeContext.Provider>
   )
 }
+
+function NavBar() {
+  return (
+    <>
+      <Button value="Home" />
+      <Button value="About" />
+    </>
+  )
+}
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  static contextType = ThemeContext;
+  render() {
+    console.log(this.context)
+    return (
+      <button style={this.context === "dark" ? { color: 'white', background: "black" } : { color: 'black' }}>{this.props.value}</button>
+    )
+  }
+}
+
+
 export default App;
