c1258111ae50a0989cd83397731bc7230f1ee03e
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-07-13 10:27:25 -0400

    splitting up app component into smaller components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
-import React, { useContext } from "react";
+import React from "react";
 import ThemeContext from "./contexts/theme";
 import UserContext from "./contexts/user";
+import NavBar from "./components/NavBar";
+
+
 
 function App() {
   return (
     <ThemeContext.Provider value="light">
-      <UserContext.Provider>
+      <UserContext.Provider value={{userName:'', pic: '', lang: ''}}>
         <h1>context</h1>
         <NavBar />
       </UserContext.Provider>
     </ThemeContext.Provider>
-  )
-}
-
-function NavBar() {
-  return (
-    <>
-      <Button value="Home" />
-      <Button value="About" />
-      <Avatar />
-    </>
-  )
-}
-
-class Button extends React.Component {
-  static contextType = ThemeContext;
-  render() {
-    console.log(this.context)
-    return (
-      <button style={this.context === "dark" ? { color: 'white', background: "black" } : { color: 'black' }}>{this.props.value}</button>
-    )
-  }
-}
-
-function Avatar() {
-  const signedInUser = useContext(UserContext);
-  return (
-    <div>
-      <a href="#profile" rel="noopener noreferal">
-        <img src={signedInUser.pic} alt="happy user" />
-      </a>
-    </div>
   )
 }
 
