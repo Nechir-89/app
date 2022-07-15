2afbae2e8f2ef2c1be893310cc6adab32604bb2f
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-07-15 09:59:09 -0400

    adding deep filter

---
+++ src/components/deepFilter/filter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+class DeepFilter extends React.Component{
+  render(){
+    return(
+      <section>
+        Deep Filter
+      </section>
+    )
+  }
+}
+
+export default DeepFilter;
---
+++ src/components/startFilter/Readme.md
@@ -0,0 +1,2 @@
+### About Feature
+This filter feature will search for all items start with the letter or string that user type.
