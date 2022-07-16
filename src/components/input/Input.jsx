239eb560eacc8b2c738e6ea739ab917720aa1833
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-07-16 10:21:30 -0400

    fixing outline color for deep filter input

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,11 +1,12 @@
 import SearchIcon from '@mui/icons-material/Search';
 import './style.css'
 // input
-export default function Input({ inputState, handlechange }) {
+export default function Input({ inputState, handlechange, filterType }) {
   return (
     <div className="input-wrapper">
       <input
         type="text"
+        className={filterType === 'deepFilter' && 'deep-filter-input'}
         value={inputState}
         onChange={handlechange}
         name="serachInput" />
